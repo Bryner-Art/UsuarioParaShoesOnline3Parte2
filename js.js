744c8c7327b10078dd18e0d0dfaf9ca7660e25d5
Bryner-Art <55173927+Bryner-Art@users.noreply.github.com>
2020-10-14 00:28:57 -0600

    update

--- js.js
+++ js.js
@@ -1,20 +1,48 @@
 
 function itemNuevo(){
-	var dpi = document.getElementById("dpi").value;
-	var nombre = document.getElementById("nombre").value;
-	var apellido = document.getElementById("apellido").value;
-	var email = document.getElementById("email").value;
-	var tel = document.getElementById("tel").value;
-	var datos = document.getElementById("zonadatos")
 
-	datos.innerHTML ="<p>" +dpi+
-						"<br>"+nombre+
-						"<br>"+apellido+
-						"<br>"+email+
-						"<br>"+tel+"</p>";
-    document.getElementById("dpi").value="";
+  function register(dpi,nombre,apellido,email,tel){
+
+  	this.dpi=dpi;
+  	this.nombre=nombre;
+  	this.apellido=apellido;
+  	this.email=email;
+  	this.tel=tel;
+
+  }
+
+	var Capdpi = document.getElementById("dpi").value;
+	var Capnombre = document.getElementById("nombre").value;
+	var Capapellido = document.getElementById("apellido").value;
+	var Capemail = document.getElementById("email").value;
+	var Captel = document.getElementById("tel").value;
+
+	newregister = new register(Capdpi,Capnombre,Capapellido,Capemail,Captel);
+   itemAgregar();
+   }
+   var basedatos= [];
+   function itemAgregar(){
+  
+   basedatos.push(newregister);
+   document.getElementById("tabla").innerHTML += "<tbody><td>"+newregister.dpi+"</td><td>"+newregister.nombre+"</td><td>"+newregister.apellido+"</td><td>"+newregister.email+"</td><td>"+newregister.tel+"</td></tbody>";
+
+   document.getElementById("dpi").value="";
     document.getElementById("nombre").value="";
     document.getElementById("apellido").value="";
     document.getElementById("email").value="";
     document.getElementById("tel").value="";
-}
+   }
+
+
+	/*datos.innerHTML +="<p>" +dpi+
+						"<br>"+nombre+
+						"<br>"+apellido+
+						"<br>"+email+
+						"<br>"+tel+"</p>";
+
+    document.getElementById("dpi").value="";
+    document.getElementById("nombre").value="";
+    document.getElementById("apellido").value="";
+    document.getElementById("email").value="";
+    document.getElementById("tel").value="";*/
+
